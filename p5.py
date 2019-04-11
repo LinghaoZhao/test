590c6be038625ef3dd7bbff104fb7cb37841ee97
zhaolinghao <972506334@qq.com>
2019-04-11 15:42:33 +0800

    new file

---
+++ p5.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 19:25:02 2019
+
+@author: 赵令浩
+"""
+
+import random
+colors = ['red','yellow','blue','green','orange','purple','black','white']
+print(random.choice(colors))
